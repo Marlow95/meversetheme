44c4761a5aaea49f5dc4cd69f6f38f2d9cd088ed
Marlow Collins <marlowcollins95@gmail.com>
2021-09-15 20:38:14 -0500

    About Us Page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,9 +1,20 @@
 import React from 'react';
 
 function About(){
+    const styles = {
+        backgroundColor:'#6A68CC', 
+        color:'white',
+        fontFamily:'Poppins',
+        borderRadius:'6px',
+        width:'80rem',
+        textAlign:'center'
+    }
     return (
         <div>
-            <h1>About</h1>
+    
+            <div style={styles}>
+                <h1>Who's Delia?</h1>
+            </div>
         </div>
 
     )
