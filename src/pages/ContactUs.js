ce65450014fa0727b17ceb38a3a95a64b141650e
Marlow Collins <marlowcollins95@gmail.com>
2021-09-16 11:20:31 -0500

    Contact Us Updates

--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -1,9 +1,48 @@
 import React from 'react';
 
 function ContactUs(){
+
+    const mainStyles = {
+        textAlign: 'center',
+        color: 'white',
+        backgroundColor: '#6A68CC', 
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center'
+    }
+
+    const contactStyles = {
+        backgroundColor: '#302EA5',
+        borderRadius: '20px',
+        width: '500px', 
+        margin: '30px',
+        padding: '20px'
+    }
+
+    const inputStyles = {
+        margin: '10px',
+        border: 'none',
+        borderRadius: '5px',
+        padding: '10px'
+    }  
+    
+    const textArea = {
+        border: 'none',
+        borderRadius: '5px',
+        padding: '10px'
+    }
+
     return (
-        <div>
+        <div style={mainStyles}>
             <h1>Contact Us</h1>
+            <form style={contactStyles}>
+                Full Name<br/>
+                <input style={inputStyles} type="text" placeholder="Name" name="fullname"/><br/>
+                E-mail<br/>
+                <input style={inputStyles} type="email" placeholder="E-mail" name="email"/><br/>
+                <textarea style={textArea} placeholder="Message" name="message"/><br/>
+                <button>Get in Touch</button>
+            </form>
         </div>
 
     )
